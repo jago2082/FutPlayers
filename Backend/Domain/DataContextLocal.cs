c48afbb011decf49d5959053b588ec71dbd8b388
Javier Penagos <jmp2082@gmail.com>
2017-09-12 06:54:06 -0500

    Creacion datacontext

---
+++ Backend/Domain/DataContextLocal.cs
@@ -0,0 +1,15 @@
+﻿using FutPlayers.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DataContextLocal: DataContext
+    {
+       
+    }
+}
